12b2689a3adf41ea29476bb0bd08f9d36f3bee5d
UfukArslan <ufuk.arslan@hes-so.ch>
2020-09-26 12:43:21 +0000

    RouteController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('tasks', [TaskController::class, 'showIndex'])->name('task.index');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('tasks', function(){
+//     return 'Task liste';
+// });
+
+// Route::get('tasks/{id}', [TaskController::class, 'showTask']);
+
+// Route::get('tasks/test/{id}', function($id) {
+//     return view('task', ['id'= $id]);
+// });
+
