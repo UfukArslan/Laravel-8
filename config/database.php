72da9e23d2a8e527799b355bca6cbaed2e9d23b7
UfukArslan <ufuk.arslan@hes-so.ch>
2020-10-08 17:43:08 +0000

    database.php

--- config/database.php
+++ config/database.php
@@ -142,6 +142,19 @@ return [
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
+
+
+    ],
+
+        'heroku' => [
+            'driver' => 'pgsql',
+            'host' => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
     ],
 
 ];
